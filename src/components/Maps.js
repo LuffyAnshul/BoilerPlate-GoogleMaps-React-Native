f7ff97586fb5dc1cb1b1010b8d24cfe6400c12e0
LuffyAnshul <anshulborawake@gmail.com>
2021-01-09 12:24:44 +0530

    Maps Updated

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Dimensions, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 
 const height = Dimensions.get('window').height;
 
@@ -10,27 +10,26 @@ const Maps = () => {
 			style={styles.map}
 			loadingEnabled={true}
 			region={{
-				latitude: 37.78825,
-				longitude: -122.4324,
+				latitude: 18.557733,
+				longitude: 73.796122,
 				latitudeDelta: 0.015,
 				longitudeDelta: 0.0121
 			}}
 		>
-			<MapView.Marker 
+			<Marker 
 				coordinate = {{
-					latitude: 37.78825,
-					longitude: -122.4324
+					latitude: 18.557733,
+					longitude: 73.796122
 				}}
-				title={"Title 1"}
-				description={"Description 1"}
+				title={"My Location"}
+				description={"Home"}
 			/>
-			<MapView.Marker 
+			<Marker 
 				coordinate = {{
-					latitude: 37.78925,
-					longitude: -122.4324
+					latitude: 18.556433,
+					longitude: 73.794750
 				}}
-				title={"Title 2"}
-				description={"Description 2"}
+				title={"Domino's Pizza"}
 			/>
 		</MapView>
 	)
